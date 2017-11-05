d0ac128f00e5b1978ba0acc8122289186fa55d56
Murad Khan <muradkhan101@gmail.com>
2017-11-04 18:18:36 -0700

    Updated wireframe and created starting file structure

---
+++ src/PhotoTransformerApp/PhotoTransformer.container.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class PhotoTransformer extends React.Component {
+  render() {
+    return (
+      <div> Thing </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import PhotoTransformer from './PhotoTransformerApp/PhotoTransformer.display';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<PhotoTransformer />, document.getElementById('root'));
 registerServiceWorker();
